f4facbfd92be625eb863ad4eb5aa535e410e6c60
noear <noear@live.cn>
2021-06-11 11:59:56 +0800

    1.4.13

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * demo1 : 展示一个 "基于轻网关的接口开发" 最基本开发结构，含单测。
 * demo2 : 在 demo1 基础上，增加 1)接口基类、2)异常处理、3)过滤器处
-* demo3 : 在 demo2 基础上，增加 1)接口状态码规范化管理、2)缓存与数据处理
+* demo3 : 在 demo2 基础上，增加 1)接口状态码规范化管理、2)缓存与数据处理 //不能启动；因为数据库之类的是错的配置
 
 
 轻网关的高级定制可以参考：[https://gitee.com/noear/srww/tree/main/srww.uapi](https://gitee.com/noear/srww/tree/main/srww.uapi)
--- demo3/src/main/java/apidemo3/controller/apis/API_cache.java
+++ demo3/src/main/java/apidemo3/controller/apis/API_cache.java
@@ -3,14 +3,16 @@ package apidemo3.controller.apis;
 import apidemo3.controller.ApiBase;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.extend.data.annotation.Cache;
 
 /**
  * @author noear 2021/6/11 created
  */
 @Component(tag = "api")
 public class API_cache extends ApiBase {
+    @Cache(seconds = 12)
     @Mapping("cache")
-    public void exec() {
-        throw new RuntimeException("随便报一下");
+    public long exec() {
+        return System.currentTimeMillis();
     }
 }
