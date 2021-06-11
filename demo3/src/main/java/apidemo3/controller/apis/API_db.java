efb32309917f5647bc5256a7b523d90cdf7b347b
noear <noear@live.cn>
2021-06-11 11:44:19 +0800

    1.4.13

--- demo3/src/main/java/apidemo3/controller/apis/API_db.java
+++ demo3/src/main/java/apidemo3/controller/apis/API_db.java
@@ -6,7 +6,7 @@ import apidemo3.model.UserModel;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.extend.validation.annotation.NotEmpty;
+import org.noear.solon.extend.validation.annotation.NotZero;
 
 import java.sql.SQLException;
 
@@ -19,7 +19,7 @@ public class API_db extends ApiBase {
     @Inject
     UserService userService;
 
-    @NotEmpty("userId")
+    @NotZero("userId")
     @Mapping("db")
     public UserModel exec(long userId) throws SQLException {
         return userService.getUser(userId);
