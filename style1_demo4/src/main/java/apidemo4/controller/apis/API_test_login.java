11f0acc5de758001ac044f40530f75a6694b25c6
noear <noear@live.cn>
2021-06-17 14:10:30 +0800

    1.5.5

--- style1_demo4/src/main/java/apidemo4/controller/apis/API_test_login.java
+++ style1_demo4/src/main/java/apidemo4/controller/apis/API_test_login.java
@@ -3,6 +3,7 @@ package apidemo4.controller.apis;
 import apidemo4.controller.ApiBase;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.Context;
 
 /**
  * @author noear 2021/6/17 created
@@ -10,7 +11,14 @@ import org.noear.solon.annotation.Mapping;
 @Component(tag = "api")
 public class API_test_login extends ApiBase {
     @Mapping("test.login")
-    public void exec() {
+    public String exec(Context ctx) {
+        //设置session
+        ctx.sessionSet("user_id", 12L);
 
+        //将session值，手动生成token并做为接口的结果输出
+        return ctx.sessionState().sessionToken();
+
+
+        //如果：server.session.state.jwt.allowIssue=true，则会自动输出
     }
 }
