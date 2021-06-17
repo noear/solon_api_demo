c43c8bac0df01b8d136af8dbcef5ac5755554cfc
noear <noear@live.cn>
2021-06-17 14:35:44 +0800

    1.5.5

---
+++ style2_demo2/src/main/java/apidemo2/controller/ApiBase.java
@@ -0,0 +1,27 @@
+package apidemo2.controller;
+
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Render;
+import org.noear.solon.core.handle.Result;
+
+/**
+ * @author noear 2021/6/11 created
+ */
+public class ApiBase implements Render {
+    @Override
+    public void render(Object obj, Context ctx) throws Throwable {
+        if (ctx.getRendered()) {
+            return;
+        }
+        //最多一次渲染
+        ctx.setRendered(true);
+
+        //如果是异常
+        if (obj instanceof Throwable) {
+            ctx.render(Result.failure("unknown error"));
+            return;
+        }
+
+        ctx.render(obj);
+    }
+}
--- style2_demo4/src/main/java/apidemo4/Config.java
+++ style2_demo4/src/main/java/apidemo4/Config.java
@@ -1,13 +1,18 @@
-package apidemo3;
+package apidemo4;
 
-import apidemo3.dso.valid.ValidatorFailureHandlerImpl;
+import apidemo4.dso.valid.LoginedCheckerImpl;
+import apidemo4.dso.valid.ValidatorFailureHandlerImpl;
 import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.cache.jedis.RedisCacheService;
+import org.noear.solon.core.handle.Context;
 import org.noear.solon.data.cache.CacheService;
 import org.noear.solon.validation.ValidatorFailureHandler;
+import org.noear.solon.validation.ValidatorManager;
+import org.noear.solon.validation.annotation.Logined;
+import org.noear.solon.validation.annotation.LoginedChecker;
 
 import javax.sql.DataSource;
 
@@ -16,10 +21,12 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class Config {
-    //增加参数验证器的自定义处理(配套接口状态码)
     @Bean
-    public ValidatorFailureHandler validatorFailureHandler(){
-        return new ValidatorFailureHandlerImpl();
+    public void validatorFailureHandler() {
+        //增加参数验证器的自定义处理(配套接口状态码)
+        ValidatorManager.setFailureHandler(new ValidatorFailureHandlerImpl());
+        //增加登录状态注解检测器支持
+        ValidatorManager.setLoginedChecker(new LoginedCheckerImpl());
     }
 
     @Bean
