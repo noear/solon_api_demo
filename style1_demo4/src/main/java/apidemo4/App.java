78a477d47381054237574886d500a563b48a8999
noear <noear@live.cn>
2023-10-15 16:26:11 +0800

    2.5.9

--- style1_demo4/src/main/java/apidemo4/App.java
+++ style1_demo4/src/main/java/apidemo4/App.java
@@ -7,6 +7,6 @@ import org.noear.solon.Solon;
  */
 public class App {
     public static void main(String[] args) {
-        Solon.start(App.class, args).onError(e -> e.printStackTrace());
+        Solon.start(App.class, args);
     }
 }
